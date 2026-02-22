833ef10ba0a0fcb9e4aa6b141f228013f20e72ee
ishupatel309 <ishupatel796@gmail.com>
2026-02-22 18:20:46 +0530

    Add files via upload

---
+++ odd_numbers.py
@@ -0,0 +1,18 @@
+odd_numbers = []
+
+for i in range(1, 101):
+    if i % 2 != 0:
+        odd_numbers.append(i)
+
+
+print("Odd numbers between 1 and 100:")
+print(odd_numbers)
+
+
+print("Minimum odd number:", min(odd_numbers))
+
+
+print("Maximum odd number:", max(odd_numbers))
+
+
+print("Sum of all odd numbers:", sum(odd_numbers))
