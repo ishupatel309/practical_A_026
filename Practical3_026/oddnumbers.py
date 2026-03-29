494f8ebc3e087d9c0b6ce216c58cf69513d29aa1
[Ishita Patel] <[ishupatel796@gmail.com]>
2026-03-29 20:01:44 +0530

    Added Practical3 folder correctly

---
+++ Practical3_026/oddnumbers.py
@@ -0,0 +1,20 @@
+# Odd numbers between 1 to 100
+
+odd_numbers = []
+
+for i in range(1, 101):
+    if i % 2 != 0:
+        odd_numbers.append(i)
+
+# Display list
+print("Odd Numbers:", odd_numbers)
+
+# Minimum odd number
+print("Minimum Odd Number:", min(odd_numbers))
+
+# Maximum odd number
+print("Maximum Odd Number:", max(odd_numbers))
+
+# Sum of odd numbers
+print("Sum of Odd Numbers:", sum(odd_numbers))
+
